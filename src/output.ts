0333fc5fd9a24f518afdc21bed0d1b06668680e2
Salman Ahmed <me@salman.so>
2024-10-21 22:27:32 +0300

    remove requires

--- src/output.ts
+++ src/output.ts
@@ -1,44 +1,52 @@
 import {
-  complexModifications,
-  ModificationParameters,
-} from './config/complex-modifications.ts'
-import { RuleBuilder } from './config/rule.ts'
-import { KarabinerConfig, Rule } from './karabiner/karabiner-config.ts'
+	complexModifications,
+	ModificationParameters,
+} from "./config/complex-modifications.ts";
+import { RuleBuilder } from "./config/rule.ts";
+import { KarabinerConfig, Rule } from "./karabiner/karabiner-config.ts";
+import path from "node:path";
+import fs from "node:fs";
+import os from "node:os";
 
 export const writeContext = {
-  karabinerConfigDir() {
-    return require('node:path').join(
-      require('node:os').homedir(),
-      '.config/karabiner',
-    )
-  },
-  karabinerConfigFile() {
-    return require('node:path').join(
-      this.karabinerConfigDir(),
-      'karabiner.json',
-    )
-  },
-  readKarabinerConfig(karabinerJsonPath?: string) {
-    return require(karabinerJsonPath ?? this.karabinerConfigFile())
-  },
-  writeKarabinerConfig(json: any, karabinerJsonPath?: string) {
-    return require('node:fs/promises').writeFile(
-      karabinerJsonPath ?? this.karabinerConfigFile(),
-      json,
-    )
-  },
-  readJson(filePath: string) {
-    return require(filePath)
-  },
-  exit(code = 0): never {
-    process.exit(code)
-  },
-}
+	karabinerConfigDir() {
+		return path.join(
+			os.homedir(),
+			".config/karabiner",
+		);
+	},
+	karabinerConfigFile() {
+		return path.join(
+			this.karabinerConfigDir(),
+			"karabiner.json",
+		);
+	},
+	readKarabinerConfig(karabinerJsonPath?: string) {
+		const content = JSON.parse(
+			fs.readFileSync(
+				karabinerJsonPath ?? this.karabinerConfigFile(),
+			).toString(),
+		);
+		return content;
+	},
+	writeKarabinerConfig(json: any, karabinerJsonPath?: string) {
+		return fs.writeFileSync(
+			karabinerJsonPath ?? this.karabinerConfigFile(),
+			json,
+		);
+	},
+	readJson(filePath: string) {
+		return JSON.parse(fs.readFileSync(filePath).toString());
+	},
+	exit(code = 0) {
+		process.exit(code);
+	},
+};
 
 export interface WriteTarget {
-  name: string
-  dryRun?: boolean
-  karabinerJsonPath?: string
+	name: string;
+	dryRun?: boolean;
+	karabinerJsonPath?: string;
 }
 
 /**
@@ -52,54 +60,55 @@ export interface WriteTarget {
  * @see https://karabiner-elements.pqrs.org/docs/json/root-data-structure/
  */
 export function writeToProfile(
-  writeTarget: '--dry-run' | string | WriteTarget,
-  rules: Array<Rule | RuleBuilder>,
-  parameters: ModificationParameters = {},
+	writeTarget: "--dry-run" | string | WriteTarget,
+	rules: Array<Rule | RuleBuilder>,
+	parameters: ModificationParameters = {},
 ) {
-  if (typeof writeTarget === 'string') {
-    writeTarget = { name: writeTarget, dryRun: writeTarget === '--dry-run' }
-  }
-  const { name, dryRun } = writeTarget
-  const jsonPath =
-    writeTarget.karabinerJsonPath ?? writeContext.karabinerConfigFile()
+	if (typeof writeTarget === "string") {
+		writeTarget = { name: writeTarget, dryRun: writeTarget === "--dry-run" };
+	}
+	const { name, dryRun } = writeTarget;
+	const jsonPath = writeTarget.karabinerJsonPath ??
+		writeContext.karabinerConfigFile();
 
-  const config: KarabinerConfig = dryRun
-    ? { profiles: [{ name, complex_modifications: { rules: [] } }] }
-    : writeContext.readKarabinerConfig(jsonPath)
+	const config: KarabinerConfig = dryRun
+		? { profiles: [{ name, complex_modifications: { rules: [] } }] }
+		: writeContext.readKarabinerConfig(jsonPath);
 
-  const profile = config?.profiles.find((v) => v.name === name)
-  if (!profile)
-    exitWithError(`⚠️ Profile ${name} not found in ${jsonPath}.\n
+	const profile = config?.profiles.find((v) => v.name === name);
+	if (!profile) {
+		exitWithError(`⚠️ Profile ${name} not found in ${jsonPath}.\n
 ℹ️ Please check the profile name in the Karabiner-Elements UI and 
     - Update the profile name at writeToProfile()
     - Create a new profile if needed
- `)
+ `);
+	}
 
-  try {
-    profile.complex_modifications = complexModifications(rules, parameters)
-  } catch (e) {
-    exitWithError(e)
-  }
+	try {
+		profile.complex_modifications = complexModifications(rules, parameters);
+	} catch (e) {
+		exitWithError(e);
+	}
 
-  const json = JSON.stringify(config, null, 2)
+	const json = JSON.stringify(config, null, 2);
 
-  if (dryRun) {
-    console.info(json)
-    return
-  }
+	if (dryRun) {
+		console.info(json);
+		return;
+	}
 
-  writeContext.writeKarabinerConfig(json, jsonPath).catch(exitWithError)
+	writeContext.writeKarabinerConfig(json, jsonPath); //.catch(exitWithError);
 
-  console.log(`✓ Profile ${name} updated.`)
+	console.log(`✓ Profile ${name} updated.`);
 }
 
 function exitWithError(err: any): never {
-  if (err) {
-    if (typeof err === 'string') {
-      console.error(err)
-    } else {
-      console.error((err as Error).message || err)
-    }
-  }
-  return writeContext.exit(1)
+	if (err) {
+		if (typeof err === "string") {
+			console.error(err);
+		} else {
+			console.error((err as Error).message || err);
+		}
+	}
+	return writeContext.exit(1);
 }
